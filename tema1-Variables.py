eb3d73780c0b9fcf3b45d0cb535d616e819801a6
IDGS-803-21000011 <paulovaladez813@gmail.com>
2024-01-12 18:48:29 -0600

    PEVG 12/01/2024 Tuplas

---
+++ tema1-Variables.py
@@ -0,0 +1,23 @@
+# Nombre de Variables
+'''
+Comentarios
+Multi
+Línea
+'''
+
+numero1 = "23"
+numero2 = "10"
+nombre = "Domingo"
+correo = "domingo@gmail.com"
+
+print(numero1 + numero2)
+
+print("La suma es: ")
+print(int(numero1) + int(numero2))
+
+sueldo = "938.284"
+valorDecimal = float(sueldo)
+print(valorDecimal)
+
+edad=238
+print(len(str(edad)))
